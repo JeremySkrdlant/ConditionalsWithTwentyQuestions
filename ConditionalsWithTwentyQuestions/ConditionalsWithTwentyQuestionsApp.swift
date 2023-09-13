1ad9fe75e28769eb1ca4fdf8ad538dcfa40fb591
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2023-09-12 18:38:09 -0600

    Initial Commit

---
+++ ConditionalsWithTwentyQuestions/ConditionalsWithTwentyQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConditionalsWithTwentyQuestionsApp.swift
+//  ConditionalsWithTwentyQuestions
+//
+//  Created by Jeremy Skrdlant on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ConditionalsWithTwentyQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
